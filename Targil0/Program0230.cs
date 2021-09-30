910d054d64c7228b60318384b1b7c5383861f9be
Miriam613 <Handesaim3A19@mbjcomp.org.il>
2021-09-30 13:03:28 +0300

    intial code + 2 welcome functions

---
+++ Targil0/Program0230.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome8214();
+            Welcome0230();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome8214();
+
+        private static void Welcome0230()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name} welcome to my first console application");
+        }
+    }
+}
