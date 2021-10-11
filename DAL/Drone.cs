470b32943528ef8a0266f90c139cd85b96975953
Miriam613 <Handesaim3A19@mbjcomp.org.il>
2021-10-11 18:40:58 +0300

    added parcle and drone classes

---
+++ DAL/Drone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public class Drone
+        {
+            public int Id{ get; set; }
+            public string Model { get; set; }
+            public weightCategories MaxWeight { get; set; }
+            public droneStatus Status { get; set; }
+            public double Battery { get; set; }
+        }
+    }
+}
---
+++ DAL/Parcel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        class Parcel
+        {
+            public int Id { get; set; }
+            public int SenderId { get; set; }
+            public int Target { get; set; }
+            public weightCategories Weight { get; set; }
+            public pritorities Pritority { get; set; }
+            public DateTime Requested { get; set; }
+            public int DroneId { get; set; }
+
+            public DateTime Scheduled { get; set; }
+
+            public DateTime PickedUp { get; set; }
+
+            public DateTime Delivered { get; set; }
+
+
+        }
+
+    }
+   
+}
