0152f5e0cc01b02391f5d1f098326e6b77ba82a7
Miriam613 <Handesaim3A19@mbjcomp.org.il>
2021-10-13 18:48:31 +0300

    gets arries functions

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DalObject;
+using IDAL.DO;
+
 namespace DalObject
 {
     public class DalObject
@@ -12,6 +14,49 @@ namespace DalObject
         {
             DataSource.Initialize();
         }
+        public Station[] GetStations()
+        {
+            int length = DataSource.stations.Length;
+            Station[] newStations = new Station[length];
+            for(int i = 0; i < length; i++)
+            {
+                newStations[i] = DataSource.stations[i];
+            }
+            return newStations;
+        }
 
+        public Drone[] GetDrones()
+        {
+            int length = DataSource.drones.Length;
+            Drone[] newDrones = new Drone[length];
+            for (int i = 0; i < length; i++)
+            {
+                newDrones[i] = DataSource.drones[i];
+            }
+            return newDrones;
+        }
+
+        public Customer[] GetCustomer()
+        {
+            int length = DataSource.customers.Length;
+            Customer[] newCustomer = new Customer[length];
+            for (int i = 0; i < length; i++)
+            {
+                newCustomer[i] = DataSource.customers[i];
+            }
+            return newCustomer;
+        }
+
+        public Parcel[] GetParcel()
+        {
+            int length = DataSource.parcels.Length;
+            Parcel[] newParcel = new Parcel[length];
+            for (int i = 0; i < length; i++)
+            {
+                newParcel[i] = DataSource.parcels[i];
+            }
+            return newParcel;
+        }
+         
     }
 }
