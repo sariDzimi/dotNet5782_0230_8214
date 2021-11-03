59703ebbab727b4e3270f8a4bda1a99197a17579
Miriam613 <Handesaim3A19@mbjcomp.org.il>
2021-11-03 18:36:25 +0200

    Logic Classes

---
+++ BL/DroneToList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        class DroneToList
+        {
+            public int Id { get; set; }
+
+            public string Model { get; set; }
+
+            public double Battery { get; set; }
+
+            public DroneStatus DroneStatus { get; set; }
+
+            public Location Location { get; set; }
+
+            public int NumberOfSendedParcel { get; set; }
+
+        }
+    }
+}
