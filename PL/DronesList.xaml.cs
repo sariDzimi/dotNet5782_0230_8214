3cbb1ee0d7a0dbe226079c0aebfdc60d08a2eba7
Miriam613 <Handesaim3A19@mbjcomp.org.il>
2021-12-07 15:40:53 +0200

    commit small changes

--- PL/DronesList.xaml.cs
+++ PL/DronesList.xaml.cs
@@ -11,8 +11,6 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using System.Collections.Generic;
-using System.Windows;
 
 namespace PL
 {
